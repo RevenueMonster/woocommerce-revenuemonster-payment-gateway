b44ffc5b37711abe39d3a67b4ca1ed12af47f525
derrick <derrick.tio@revenuemonster.my>
2026-09-04 17:30:36 +0800

    feat/card-direct: add query on checkout page to check merchant subscription

--- revenuemonster-gateway.php
+++ revenuemonster-gateway.php
@@ -5,7 +5,7 @@
  * Description: Accept all major Malaysia e-wallet, such as TnG eWallet, Boost, Maybank QRPay & credit cards. Fast, seamless, and flexible.
  * Author: RevenueMonster
  * Author URI: https://revenuemonster.my/
- * Version: 1.0.10
+ * Version: 1.0.11
  * WC requires at least: 3.0
  * WC tested up to: 8.2
  * Requires Plugins: woocommerce
@@ -18,7 +18,8 @@
  */
 
 defined('ABSPATH') || die('Missing global variable ABSPATH');
-
+// Keep in sync with the "Version:" header above;
+define('WC_REVENUEMONSTER_VERSION', '1.0.11');
 define('WC_REVENUEMONSTER_FILE', __FILE__);
 define('WC_REVENUEMONSTER_PATH', plugin_dir_path(__FILE__));
 define('WC_REVENUEMONSTER_URL', plugin_dir_url(__FILE__));
